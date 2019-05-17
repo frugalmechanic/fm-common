7863d0a670c9b0ed23e85a39cdcee53ae00a2228
Tim Underwood <timunderwood@gmail.com>
2019-05-17 14:34:47 -0700

    RichLocalDate - Added toYYYYMMDD and variants

--- shared/src/main/scala/fm/common/rich/RichLocalDate.scala
+++ shared/src/main/scala/fm/common/rich/RichLocalDate.scala
@@ -19,4 +19,51 @@ import java.time.LocalDate
 
 final class RichLocalDate(val date: LocalDate) extends AnyVal with Ordered[LocalDate] {
   def compare(that: LocalDate): Int = date compareTo that
+
+  def toYYYYMMDDOption: Option[String] = if (isWithinYYYYRange) Some(toYYYYMMDD) else None
+
+  /**
+   * Returns the YYYYMMDD string representation of the date.  Works for the range: 0000-01-01 to 9999-12-31
+   */
+  def toYYYYMMDD: String = {
+    yyyyRangeCheck()
+
+    val year: Int = date.getYear
+    val month: Int = date.getMonthValue
+    val day: Int = date.getDayOfMonth
+
+    val sb: java.lang.StringBuilder = new java.lang.StringBuilder(8)
+
+    if (year < 9) sb.append('0')
+    if (year < 99) sb.append('0')
+    if (year < 999) sb.append('0')
+    sb.append(year)
+
+    if (month < 9) sb.append('0')
+    sb.append(month)
+
+    if (day < 9) sb.append('0')
+    sb.append(day)
+
+    sb.toString()
+  }
+
+  def toYYYYMMDDIntOption: Option[Int] = if (isWithinYYYYRange) Some(toYYYYMMDDInt) else None
+
+  /**
+   * Returns the YYYYMMDD int representation of the date.  Works for the range: 0000-01-01 to 9999-12-31
+   */
+  def toYYYYMMDDInt: Int = {
+    yyyyRangeCheck()
+    date.getYear * 10000 + date.getMonthValue * 100 + date.getDayOfMonth
+  }
+
+  private def isWithinYYYYRange: Boolean = {
+    val year: Int = date.getYear
+    year >= 0 && year <= 9999
+  }
+
+  private def yyyyRangeCheck(): Unit = {
+    if (!isWithinYYYYRange) throw new IllegalArgumentException("toYYYYMMDD is only supported for years <= 9999 and >= 0")
+  }
 }
