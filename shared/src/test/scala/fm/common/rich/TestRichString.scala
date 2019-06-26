frugalmechanic/fm-common

commit d7aac864cf67bcee9815955dc34be2838b3ecc3d

Switching to using OptionCache

shared/src/main/scala/fm/common/rich/RichString.scala
@@ -15,8 +15,7 @@
  */
 package fm.common.rich
 
-import fm.common.{Normalize, ThreadLocalHashMap}
-import fm.common.Implicits.toRichTraversableOnce
+import fm.common.{Normalize, OptionCache, ThreadLocalHashMap}
 import java.io.File
 import java.math.{BigDecimal, BigInteger}
 import java.text.{DecimalFormat, NumberFormat, ParseException}
@@ -25,26 +24,16 @@ import java.util.function.IntConsumer
 import scala.util.matching.Regex
 
 object RichString {
-  private[this] val booleanLookupMap: Map[String,Boolean] = Vector(
-    "true" -> true,
-    "yes" -> true,
-    "t" -> true,
-    "y" -> true,
-    "1" -> true,
-    "false" -> false,
-    "no" -> false,
-    "f" -> false,
-    "n" -> false,
-    "0" -> false
-  ).toUniqueHashMap
-  
   def parseBoolean(s: String): Option[Boolean] = {
     if (null == s) return None
-    
+
     val lower: String = s.trim.toLowerCase
     if (lower == "") return None
-    
-    booleanLookupMap.get(lower)
+
+    lower match {
+      case "true" | "t" | "yes" | "y" | "1" => OptionCache.valueOf(true)
+      case "false" | "f" | "no" | "n" | "0" => OptionCache.valueOf(false)
+    }
   }
 
   // NumberFormat.getInstance(locale) is expensive (and not thread-safe) so we need to cache it
@@ -96,11 +85,11 @@ final class RichString(val s: String) extends AnyVal {
    */
   def requireTrailing(trail: String): String = if (s.endsWith(trail)) s else s+trail
   
-  def toBooleanOption: Option[Boolean] = try { Some(java.lang.Boolean.valueOf(s)) } catch { case _: NumberFormatException => None }
-  def toByteOption:    Option[Byte]    = try { Some(java.lang.Byte.valueOf(s))    } catch { case _: NumberFormatException => None }
-  def toShortOption:   Option[Short]   = try { Some(java.lang.Short.valueOf(s))   } catch { case _: NumberFormatException => None }
-  def toIntOption:     Option[Int]     = try { Some(java.lang.Integer.valueOf(s)) } catch { case _: NumberFormatException => None }
-  def toLongOption:    Option[Long]    = try { Some(java.lang.Long.valueOf(s))    } catch { case _: NumberFormatException => None }
+  def toBooleanOption: Option[Boolean] = try { OptionCache.valueOf(java.lang.Boolean.valueOf(s)) } catch { case _: NumberFormatException => None }
+  def toByteOption:    Option[Byte]    = try { OptionCache.valueOf(java.lang.Byte.valueOf(s))    } catch { case _: NumberFormatException => None }
+  def toShortOption:   Option[Short]   = try { OptionCache.valueOf(java.lang.Short.valueOf(s))   } catch { case _: NumberFormatException => None }
+  def toIntOption:     Option[Int]     = try { OptionCache.valueOf(java.lang.Integer.valueOf(s)) } catch { case _: NumberFormatException => None }
+  def toLongOption:    Option[Long]    = try { OptionCache.valueOf(java.lang.Long.valueOf(s))    } catch { case _: NumberFormatException => None }
   def toFloatOption:   Option[Float]   = try { Some(java.lang.Float.valueOf(s))   } catch { case _: NumberFormatException => None }
   def toDoubleOption:  Option[Double]  = try { Some(java.lang.Double.valueOf(s))  } catch { case _: NumberFormatException => None }
   

shared/src/main/scala/fm/common/rich/RichBooleanOption.scala
@@ -15,14 +15,11 @@
  */
 package fm.common.rich
 
-object RichBooleanOption {
-  private val TRUE = Some(true)
-  private val FALSE = Some(false)
-}
+import fm.common.OptionCache
 
 final class RichBooleanOption(val opt: Option[Boolean]) extends AnyVal {
   /**
    * Like String.intern but for Option[Boolean]
    */
-  def intern: Option[Boolean] = if (opt.isEmpty) None else if (opt.get) RichBooleanOption.TRUE else RichBooleanOption.FALSE
+  def intern: Option[Boolean] = if (opt.isEmpty) None else OptionCache.valueOf(opt.get)
 }

commit b3892b4bef1bb37b0eba0ac491de58adb514be8f

RichString.parseBoolean should return None if a boolean value cannot be parsed from the string
This was broken in commit d7aac864cf67bcee9815955dc34be2838b3ecc3d ("Switching to using OptionCache") and existing tests did not catch it.

shared/src/main/scala/fm/common/rich/RichString.scala
@@ -33,6 +33,7 @@ object RichString {
     lower match {
       case "true" | "t" | "yes" | "y" | "1" => OptionCache.valueOf(true)
       case "false" | "f" | "no" | "n" | "0" => OptionCache.valueOf(false)
+      case _ => None
     }
   }
 

shared/src/test/scala/fm/common/rich/TestRichString.scala
@@ -58,10 +58,14 @@ final class TestRichString extends FunSuite with Matchers {
 
   test("toBoolean") {
     "true".toBoolean shouldBe true
+    "TrUe".toBoolean shouldBe true
     "false".toBoolean shouldBe false
+    "fAlSe".toBoolean shouldBe false
 
     an [IllegalArgumentException] shouldBe thrownBy { (null: String).toBoolean }
     an [IllegalArgumentException] shouldBe thrownBy { "".toBoolean }
+    an [IllegalArgumentException] shouldBe thrownBy { " true".toBoolean }
+    an [IllegalArgumentException] shouldBe thrownBy { "false ".toBoolean }
     an [IllegalArgumentException] shouldBe thrownBy { "t".toBoolean }
     an [IllegalArgumentException] shouldBe thrownBy { "f".toBoolean }
     an [IllegalArgumentException] shouldBe thrownBy { "yes".toBoolean }
@@ -76,15 +80,21 @@ final class TestRichString extends FunSuite with Matchers {
     (null: String).parseBoolean shouldBe None
     "".parseBoolean shouldBe None
     "\n".parseBoolean shouldBe None
+    "\t".parseBoolean shouldBe None
+    " ".parseBoolean shouldBe None
+    " foo ".parseBoolean shouldBe None
+    " bar ".parseBoolean shouldBe None
 
     "t".parseBoolean shouldBe Some(true)
     "true".parseBoolean shouldBe Some(true)
+    " \nTrUe\n ".parseBoolean shouldBe Some(true)
     "1".parseBoolean shouldBe Some(true)
     "y".parseBoolean shouldBe Some(true)
     "yes".parseBoolean shouldBe Some(true)
 
     "f".parseBoolean shouldBe Some(false)
     "false".parseBoolean shouldBe Some(false)
+    " \tfALSe\t ".parseBoolean shouldBe Some(false)
     "0".parseBoolean shouldBe Some(false)
     "n".parseBoolean shouldBe Some(false)
     "no".parseBoolean shouldBe Some(false)